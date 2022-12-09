5db57ca06c09214c4055aa8de78e094f1349acc3
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2022-12-09 16:03:57 +0100

    Merge
    Remember to fix close button

--- get_set_info.php
+++ get_set_info.php
@@ -2,22 +2,39 @@
     $set_id = $_POST[id];
     $connection = mysqli_connect("mysql.itn.liu.se", "lego", "", "lego");
     $result = mysqli_query($connection, 
-        "SELECT inventory.ItemtypeID, inventory.ItemID, sets.Setname, sets.SetID, sets.Year, inventory.ColorID, images.has_jpg, parts.Partname
+        "SELECT inventory.ItemtypeID, inventory.ItemID, sets.Setname, sets.SetID, sets.Year, inventory.ColorID, images.has_jpg, images.has_largejpg, parts.Partname
         FROM sets, parts, inventory, images 
         WHERE sets.SetID = '$set_id' AND sets.SetID = inventory.SetID AND inventory.ItemID = parts.PartID AND inventory.ItemID = images.ItemID AND inventory.ColorID = images.ColorID LIMIT 3000");
-
-    //OR PartID = '$search_name'
 
     $fileName = "$row[ItemtypeID]/$row[ColorID]/$row[ItemID].gif";
                 
     $filePath = "http://www.itn.liu.se/~stegu76/img.bricklink.com/";
 
     $a = mysqli_fetch_array($result);
-    print("<h1> SetName: $a[Setname] </h1>");
-    print("<h3> SetID: $a[SetID] </h3>");   
-    print("<h3> Release Year: $a[Year] </h3>");
-    print("<h4> Bricks used: </h4>");
-    //print("<p>");
+    print("<div class='flexbox-container-header'>");
+
+    // Modal header image
+    $SL="SL";
+    $fileType = "gif";    
+    if ($a[has_largejpg])
+    {
+        $fileType = "jpg";
+    }
+
+    print("<div class='flex-element-header'>");    
+    print("<img src='$filePath$SL/$a[SetID].$fileType' alt='$filePath$SL/$a[SetID].$fileType' class='sets_img'>");
+    print("</div>");
+
+    print("<div class='flex-element-header'>");
+    print("<h1 class='modal-header-text'> $a[Setname] </h1>");
+    print("<p class='modal-header-info-text'> ID: $a[SetID] </p>");   
+    print("<p class='modal-header-info-text'> Release Year: $a[Year] </p>");
+    print("</div>");
+
+    print("</div>");
+
+    // Flexbox container
+    print("<div class='flexboxContainer'>");
     while ($row = mysqli_fetch_array($result)) 
     {
 
@@ -29,7 +46,13 @@
         }
 
         $fileName = "$row[ItemtypeID]/$row[ColorID]/$row[ItemID].$fileType";
-        print("<p> $row[Partname] </p>");
+        // Brick img and name of brick div
+        print("<div class='flex-element'>");
         print("<img src='$filePath$fileName' alt='$filePath$fileName' class='sets_img'>");
+        print("<p class='modal-text'> $row[Partname] </p>");
+        print("</div>");
     }
+    print("</div>");
+
+   
 ?>
